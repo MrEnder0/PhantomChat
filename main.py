dc5a464fdc8e4b80a6aa7e6e80371ed4d82d74c2
Mr.Ender <72358826+MrEnder0@users.noreply.github.com>
2022-04-08 17:09:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -81,5 +81,9 @@ def robots():
 def page_not_found(error):
     return render_template('404.html'), 404
 
+@app.errorhandler(500)
+def page_not_found(error):
+    return render_template('500.html'), 500
+
 if __name__ == '__main__':
     app.run(host= '0.0.0.0', port=80, debug=False)

8209c803c9b827500b3f6a5c98fad81c59694789
Mr.Ender <72358826+MrEnder0@users.noreply.github.com>
2022-05-26 19:12:53 -0500

    Console info changes

---
+++ discord_rpc.py
@@ -0,0 +1,20 @@
+from pypresence import Presence 
+import time, psutil
+
+start = int(time.time())
+app_id = "979521390715752528"
+RPC = Presence(app_id)
+RPC.connect()
+
+print("[#] Discord RPC connected!")
+while True:
+    RPC.update(
+        large_image = "icon",
+        large_text = "PhantomChat",
+        details = "Hosting a hub",
+        state = f'Cpu ussage: {str(psutil.cpu_percent())}%',
+        start = start,
+        buttons = [{"label": "Check out the project", "url": "https://github.com/MrEnder0/PhantomChat"}]
+    )
+    print(f'[#] Updated with {psutil.cpu_percent()}% cpu usage')
+    time.sleep(15)
